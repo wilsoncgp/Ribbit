57764028feef76fb7fdb737141d3890957b23fdc
Liam Wilson <wilsoncgp@gmail.com>
2014-10-02 18:44:24 +0100

    Added Error Dialog to the Sign Up activity when user is missing information from the text fields.

--- app/src/main/java/com/example/liam/ribbit/MainActivity.java
+++ app/src/main/java/com/example/liam/ribbit/MainActivity.java
@@ -1,25 +1,23 @@
 package com.example.liam.ribbit;
 
-import java.util.Locale;
-
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.parse.ParseAnalytics;
+
+import java.util.Locale;
 
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
--- app/src/main/java/com/example/liam/ribbit/RibbitApplication.java
+++ app/src/main/java/com/example/liam/ribbit/RibbitApplication.java
@@ -3,18 +3,12 @@ package com.example.liam.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
-/**
- * Created by Liam on 02/10/2014.
- */
 public class RibbitApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "FU8AY2uZiDu1cDMnNuZNGZPsWPgbWhjLYep1tBeD", "yrI2mmE9YAmdW5g4OVQbIQ2Puv6fAWn7PMg8WDeO");
 
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+
     }
 }
